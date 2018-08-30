6f538521e89b183ff0f3c819a27f57693ea6b542
Charles de Bourcy <charlesdebourcy@czipa1osx177.attlocal.net>
2018-08-29 20:12:17 -0700

    plan for labeling

---
+++ idseq_dag/warehouse/generate_cami_labels.py
@@ -0,0 +1,3 @@
+# go through ~/cdebourcy/cami/short_read/2017.12.04_18.56.22_sample_10/reads/reads_mapping.tsv.gz
+# and count occurrences of each taxid.
+# taxid indicated for each read can be any level (family, genus, ...) -- make sure to record counts for the level indicated and above.
