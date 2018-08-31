06ab0dccfd620f1afd1ad781fc9b4ce536620f34
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-08-31 16:44:21 -0700

    WIP count comparison figure

---
+++ idseq_dag/warehouse/compare_cami_idseq.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import idseq_dag.util.command as command
+import sample_lists
+
+def main():
+    warehouse_dir = "/mnt/idseq/warehouse"
+    figure_dir = f"{warehouse_dir}/figures"
+    command.execute(f"mkdir -p {figure_dir}")
+    idseq_predictions = f"{warehouse_dir}/idseq_predictions.csv"
+    cami_labels = f"{warehouse_dir}/cami_labels.csv"
+    idseq_df = pd.read_csv(idseq_predictions, index_col=[0])
+    cami_df = pd.read_csv(cami_labels, index_col=[0])
+    for sample_name in sample_lists.CAMI_Airways_sample_names:
+        clean_sample_name = sample_lists.clean_name(sample_name)
+        idseq_genus = idseq_df.loc[idseq_df['sample_name'] == clean_sample_name & 
+                                   idseq_df['tax_level'] == 2 & idseq_df['taxid'] > 0]
+        cami_genus = cami_df.loc[cami_df['sample_name'] == clean_sample_name &
+                                 cami_df['tax_level'] == 2 & cami_df['taxid'] > 0]
+        df = pd.merge(idseq_genus, cami_genus, how='outer', on='taxid', suffixes=('_idseq', '_cami'))
+        df = df.fillna(0)
+        plt.plot('count_idseq', 'count_cami', data=df)
+        plt.save_fig(f"{figure_dir}/{clean_sample_name}.png", format="png")
+
+if __name__ == "__main__":
+    main()
