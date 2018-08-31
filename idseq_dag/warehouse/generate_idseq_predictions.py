b1864bbe1c4e59e1b9ad49b0af866f74dcf55175
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-08-31 14:50:51 -0700

    WIP idseq predictions

--- idseq_dag/warehouse/generate_idseq_predictions.py
+++ idseq_dag/warehouse/generate_idseq_predictions.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import glob
 import pandas as pd
 
@@ -12,12 +13,15 @@ def main():
     scratch_dir = f"{warehouse_dir}/tmp"
     result_file = f"{warehouse_dir}/idseq_predictions.csv"
     command.execute(f"rm -rf {scratch_dir}; mkdir -p {scratch_dir}")
-    reports_dir = s3.fetch_from_s3("s3://idseq-samples-test/cami_airways_reports.tar.gz",
-                                   scratch_dir, auto_unzip = True, auto_untar = True)
-    df = pd.DataFrame(columns=['sample_name', 'taxid', 'NT_r', 'NR_r', 'count'])
+    s3_reports_archive = "s3://idseq-samples-test/cami_airways_reports.tar.gz"
+    reports_archive = s3.fetch_from_s3(s3_reports_archive, scratch_dir)
+    reports_dir = reports_archive.rsplit(".", 2)[0]
+    command.execute(f"mkdir {reports_dir}; tar xf {reports_archive} -C {reports_dir}")
+    print(reports_dir)
+    df = pd.DataFrame()
     for report_file in glob.glob(f"{reports_dir}/*"):
         print(f"Starting to process {report_file}")
-        report_df = pd.read_csv(report_file, usecols=["tax_id", "NT_r", "NR_r"])
+        report_df = pd.read_csv(report_file, usecols=["tax_id", "tax_level", "NT_r", "NR_r"])
         report_df['count'] = report_df.apply(lambda row: (row["NT_r"] + row["NR_r"]) / 2.0, axis = 1)
         report_df['sample_name'] = sample_lists.clean_name(os.path.splitext(os.path.basename(report_file))[0])
         report_df.rename(columns={'tax_id': 'taxid'}, inplace=True)
