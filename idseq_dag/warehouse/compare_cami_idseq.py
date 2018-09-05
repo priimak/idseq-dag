d9472b1b4fb2c1718c85e16e51eb4a0801cbebe7
Charles de Bourcy <charlesdebourcy@czipa1osx177.local>
2018-09-04 18:23:58 -0700

    fix bugs

--- idseq_dag/warehouse/compare_cami_idseq.py
+++ idseq_dag/warehouse/compare_cami_idseq.py
@@ -19,10 +19,12 @@ def main():
     cami_df = pd.read_csv(cami_labels, index_col=[0])
     for sample_name in sample_lists.CAMI_Airways_sample_names:
         clean_sample_name = sample_lists.clean_name(sample_name)
-        idseq_genus = idseq_df.loc[idseq_df['sample_name'] == clean_sample_name & 
-                                   idseq_df['tax_level'] == 2 & idseq_df['taxid'] > 0]
-        cami_genus = cami_df.loc[cami_df['sample_name'] == clean_sample_name &
-                                 cami_df['tax_level'] == 2 & cami_df['taxid'] > 0]
+        idseq_genus = idseq_df.loc[(idseq_df['sample_name'] == clean_sample_name) & 
+                                   (idseq_df['tax_level'] == 2) &
+                                   (idseq_df['taxid'] > 0)]
+        cami_genus = cami_df.loc[(cami_df['sample_name'] == clean_sample_name) &
+                                 (cami_df['tax_level'] == 2) &
+                                 (cami_df['taxid'] > 0)]
         df = pd.merge(idseq_genus, cami_genus, how='outer', on='taxid', suffixes=('_idseq', '_cami'))
         df = df.fillna(0)
         rms = np.sqrt(mean_squared_error(df['count_cami'], df['count_idseq']))
